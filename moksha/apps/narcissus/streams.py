efe78f4a1f43cda88fafd027295003972a114882
Ralph Bean <ralph.bean@gmail.com>
2011-04-15 11:58:33 -0400

    Random IP Producer

--- moksha/apps/narcissus/streams.py
+++ moksha/apps/narcissus/streams.py
@@ -1,35 +1,14 @@
 from datetime import timedelta, datetime
 from moksha.api.hub.producer import PollingProducer
 
-import simplejson
-import geojson
 import random
 
-class NarcissusStream(PollingProducer):
+class RandomIPProducer(PollingProducer):
     frequency = timedelta(seconds=1.5)
-    topic = 'log2latlon'
-    jsonify = True
-
-    def produce_random_geojson(self):
-        lat, lon = 37.775, -122.4183333  # SF Bay Area
-        mod = lambda x : x + random.random() * 0.05 - 0.025
-
-        json = geojson.FeatureCollection(
-            features=[
-                geojson.Feature(
-                    geometry=geojson.Point([mod(lon), mod(lat)])
-                )
-            ]
-        )
-
-        # This is crazy, I know.  Geojson is weird and I'm not sure how it plays
-        # with moksha's jsonify=True stuff above (it breaks).
-        return simplejson.loads(geojson.dumps(json))
-
+    topic = 'httpdlight_http_rawlogs'
 
     def poll(self):
         """ This method is called by the MokshaHub reactor every `frequency` """
 
-        json = self.produce_random_geojson()
-        self.send_message(self.topic, json)
-
+        msg = '%i.%i.%i.%i' % (*[random.randint(0,255) for i in range(4)])
+        self.send_message(self.topic, msg)
